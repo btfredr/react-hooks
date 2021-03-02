c2c2d0ab476096ec2393e6683429b86315ea03fd
Fredrik <fredrikf2000@gmail.com>
2021-03-02 14:00:06 +0100

    trying to make an interactive form to change value

--- src/components/Order.js
+++ src/components/Order.js
@@ -13,26 +13,31 @@ const Order = () => {
         setOrder(
             // Passing the user object into a callback function
             order => (
-                // Here I'm using the spread operator "..." to make a copy of the user object.
-                // what my setUser function sees at this point is:
-                // setUser({name: 'John Doe',email: 'john@email.com',age: 28})
-                { ...order },
-                // Now i change the name value of the copied object to "Nathan"
-                // This is what setUser sees in the end:
-                // setUser({name: 'Nathan',email: 'john@email.com',age: 28})
-                { item: 'Hello', price: 1337 }
+                { ...order, item: 'Hello', price: 1337 }
             )
         );
     };
 
     return (
-        <>
-            <h1>{order.id}</h1>
-            <h1>{order.item}</h1>
-            <h1>{order.price}</h1>
-            <h1>{order.stock}</h1>
-            <button onClick={updateOrder}>Update Order</button>
-        </>
+        <div className="form">
+            <div className="form-item">
+                <div>
+                    <label>{order.item}</label>
+                </div>
+                <input
+                    type="text" value={setOrder.item}>
+                </input>
+            </div>
+            <div className="form-item">
+                <div>
+                    <label>${order.price}</label>
+                </div>
+                <input
+                    type="number" value={setOrder.price}>
+                </input>
+            </div>
+            <button className="form-button" onClick={updateOrder}>Update Order</button>
+        </div>
     )
 };
 
